9bd30e04349b5b2388158608beb92483e9bc28c2
АнтонСтепанюгин <Tony_Montana>
2019-07-24 16:47:03 +0300

    Дополнил класс "Hello" вывод новой строки

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,11 @@
+import java.util.Date;
+
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Ляськи-масяськи");
+
+      Date date = new Date();
+      System.out.println(date);
   }
 }
