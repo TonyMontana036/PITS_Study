915eca07956ca78bd466cb4f7f746e1fa1587d2a
АнтонСтепанюгин <Tony_Montana>
2019-08-02 13:14:50 +0300

    Шаг 2 к крестикам добавил 2 новых класса Новые классы Field Game Добавил переменные

---
+++ src/main/java/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+
+    Player palyer1 = new Player();
+    Player palyer2 = new Player();
+    Field field = new Field();
+    String name;
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,4 +2,10 @@ public class Player {
 
   String name;
   String figure;
+  byte age;
+  String mail;
+  String adress;
+  short score;
+  String nick;
+  String uuid;
 }
