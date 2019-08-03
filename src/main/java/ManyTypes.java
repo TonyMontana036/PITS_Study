75ff1db1e05ecda3d2fdb0c0c9303494d23fe6e0
TonyMontana036 <mudaq@mail.ru>
2019-08-03 17:46:51 +0300

    Домашнее задание переменные Создал класс для переенных Заполнил переменными

---
+++ src/main/java/ManyTypes.java
@@ -0,0 +1,25 @@
+public class ManyTypes {
+  public static void main(String[] args) {
+
+
+    boolean tumbler;
+
+    Byte a0 = Byte.MIN_VALUE;
+
+    short a1 = Short.MAX_VALUE;
+
+    char a2 = '\u0031';
+
+    int a3 = Integer.MIN_VALUE;
+
+    long a4 = Long.MIN_VALUE;
+
+    Float a5 = Float.POSITIVE_INFINITY;
+
+    Double a6 = Double.NEGATIVE_INFINITY;
+
+    String someName;
+
+    String mail;
+  }
+}
